928f2e8e37e9e51b0bf21cb9516100ff213314b0
abc <abc@macdeMac-mini.local>
2018-11-06 09:49:59 +0800

    update

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var express = require('express');
 var app = express();
 var app = require('express')();
 var bodyParser = require('body-parser');
-var multer = require('multer'); // v1.0.5
-var upload = multer(); // for parsing multipart/form-data
+// var multer = require('multer'); // v1.0.5
+// var upload = multer(); // for parsing multipart/form-data
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
